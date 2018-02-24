628c26e7678533033ebae76ac9f6dd6198e9c39a
VasilievPV <pv_vasiliev@hotmail.com>
2018-02-24 11:31:20 +0200

    Writer
    Adding "write" static method

--- src/application/Writer.java
+++ src/application/Writer.java
@@ -9,18 +9,34 @@ public final class Writer
 	public static void write(String filePath, List<Drink> drinkRange)
 	{
 		StringBuilder sb = new StringBuilder();
+		sb.append("drinkRange Data Base" + System.lineSeparator());
+		
 		for(Drink d : drinkRange)
 		{
+			try
+			{
+				AlcoholDrink ad = (AlcoholDrink)d;
+				System.out.println("this is alco drink;");
+				sb.append(ad.toString());
+			}
+			catch(ClassCastException e)
+			{				
+				System.out.println("this is unalco drink;");
+				UnAlñoholDrink uad = (UnAlñoholDrink)d;
+				sb.append(uad.toString());
+			}
 			
 		}
 		
 		try 
 		{
 			FileWriter fw = new FileWriter(filePath);
+			fw.write(sb.toString());
 		}
 		catch (IOException e)
 		{
 			e.printStackTrace();
+			System.out.println("Write failed!");
 		}
 		
 	}
