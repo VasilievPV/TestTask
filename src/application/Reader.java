7626136858420a0e28f3597814cebbc4503483d3
VasilievPV <pv_vasiliev@hotmail.com>
2018-02-24 17:45:51 +0200

    Reader changed

--- src/application/Reader.java
+++ src/application/Reader.java
@@ -1,12 +1,26 @@
 package application;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public final class Reader
 {
 	public static List<Drink> read(String filePath)
 	{
-		
+		List<Drink> tempList = new ArrayList<Drink>();
+		try(Scanner in = new Scanner(new File(filePath)))
+		{
+			//getFromFile(in, arr);
+		}
+		catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
 		
 		return null;
 	}
